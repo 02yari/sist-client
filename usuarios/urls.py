4899710ac9c32556472b01bb84dc3e409585f201
Yarielsy <yarigonza0294@gmail.com>
2025-12-16 18:19:54 -0500

    CRUD de usuarios con permisos #11 admin #

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 from .views import CustomLoginView, CustomLogoutView
 
-app_name = 'usuarios'
 
+from .views import (
+    UsuarioListView,
+    UsuarioCreateView,
+    UsuarioUpdateView,
+    UsuarioDeleteView,
+)
+app_name = 'usuarios'
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('', UsuarioListView.as_view(), name='listar'),
+    path('crear/', UsuarioCreateView.as_view(), name='crear'),
+    path('editar/<int:pk>/', UsuarioUpdateView.as_view(), name='editar'),
+    path('eliminar/<int:pk>/', UsuarioDeleteView.as_view(), name='eliminar'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -5,6 +5,13 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
+#vistas CRUD
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Usuario
+from .forms import UsuarioForm
+from .mixins import AdminRequiredMixin
+
 
 class CustomLoginView(LoginView):
     template_name = 'usuarios/login.html'
@@ -20,3 +27,36 @@ class CustomLoginView(LoginView):
 
 class CustomLogoutView(LogoutView):
     next_page = reverse_lazy('usuarios:login')
+
+
+#vistas crud
+class UsuarioListView(LoginRequiredMixin, AdminRequiredMixin, ListView):
+    model = Usuario
+    template_name = 'usuarios/usuarios_list.html'
+    context_object_name = 'usuarios'
+
+
+class UsuarioCreateView(LoginRequiredMixin, AdminRequiredMixin, CreateView):
+    model = Usuario
+    form_class = UsuarioForm
+    template_name = 'usuarios/usuario_form.html'
+    success_url = reverse_lazy('usuarios:listar')
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.set_password('12345678')  # contraseña inicial
+        user.save()
+        return super().form_valid(form)
+
+
+class UsuarioUpdateView(LoginRequiredMixin, AdminRequiredMixin, UpdateView):
+    model = Usuario
+    form_class = UsuarioForm
+    template_name = 'usuarios/usuario_form.html'
+    success_url = reverse_lazy('usuarios:listar')
+
+
+class UsuarioDeleteView(LoginRequiredMixin, AdminRequiredMixin, DeleteView):
+    model = Usuario
+    template_name = 'usuarios/usuario_confirm_delete.html'
+    success_url = reverse_lazy('usuarios:listar')
