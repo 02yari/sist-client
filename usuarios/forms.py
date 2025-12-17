5062a4c26cea525f08f6300c8d4fcabf65e239cf
Yarielsy <yarigonza0294@gmail.com>
2025-12-16 21:50:15 -0500

    Seguridad y Validaciones #12

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 from .models import Usuario
+from django.core.exceptions import ValidationError
+import re
 
 class UsuarioForm(forms.ModelForm):
-    class Meta:
+     password = forms.CharField(
+        widget=forms.PasswordInput,
+        required=False,
+        help_text="Dejar vacío si no quieres cambiar la contraseña"
+    )
+     
+class Meta:
         model = Usuario
         fields = [
             'username',
@@ -12,3 +20,28 @@ class UsuarioForm(forms.ModelForm):
             'rol',
             'is_active',
         ]
+
+def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if Usuario.objects.exclude(pk=self.instance.pk).filter(email=email).exists():
+            raise ValidationError("Este correo ya está en uso.")
+        return email
+
+def clean_password(self):
+        password = self.cleaned_data.get('password')
+        if password:
+            # Validación de contraseña segura: al menos 8 caracteres, 1 mayúscula, 1 número
+            if len(password) < 8 or not re.search(r'[A-Z]', password) or not re.search(r'\d', password):
+                raise ValidationError(
+                    "La contraseña debe tener al menos 8 caracteres, una mayúscula y un número."
+                )
+        return password
+
+def save(self, commit=True):
+        user = super().save(commit=False)
+        password = self.cleaned_data.get('password')
+        if password:
+            user.set_password(password)
+        if commit:
+            user.save()
+        return user
